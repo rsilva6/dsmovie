1cf284a7355979c5c066a522fa8c045b27819b85
Ramon Jose Silva <joseph.s.ru@gmail.com>
2022-05-09 17:37:43 -0300

    Routes

---
+++ frontend/src/Pages/Form/index.tsx
@@ -0,0 +1,7 @@
+function Form() {
+    return (
+        <h1>Formulário</h1>
+    );
+}
+
+export default Form
---
+++ frontend/src/Pages/Listing/index.tsx
@@ -0,0 +1,7 @@
+function Listing() {
+    return (
+        <h1>Listagem</h1>
+    );
+}
+
+export default Listing
