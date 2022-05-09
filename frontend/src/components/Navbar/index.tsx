83419fca3a03d5d597ad85dac79f586abae31dca
Ramon Jose Silva <joseph.s.ru@gmail.com>
2022-05-09 17:23:50 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import './styles.css';
+
+function Navbar() {
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                <h1>DS Movie</h1>
+                <a href="https://github.com/rsilva6/dsmovie" target="_blank">
+                    <div className="dsmovie-contact-container">
+                        <GithubIcon />
+                        <p className="dsmovie-contact-link">/rsilva6</p>
+                    </div>
+                </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar
