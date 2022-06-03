7a24b87618b970736a893480acfb503b8730ee08
Ramon Jose Silva <joseph.s.ru@gmail.com>
2022-06-03 15:29:54 -0300

    useState, useEffect

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,12 +1,21 @@
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 import axios from 'axios';
+import { useEffect, useState } from "react";
 import { BASE_URL } from "utils/requests";
+import { MoviePage } from "types/movie";
 
 function Listing() {
 
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-    .then(response => {console.log(response)});
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=0`)
+            .then(response => {
+                const data = response.data as MoviePage
+                setPageNumber(data.number);
+            });
+    }, []);
     
     return (
         <>
